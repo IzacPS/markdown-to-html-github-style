ee2d1ff3083512d11d5dbf6f7aa1137d80d19820
IzacPS <izaccknd@gmail.com>
2024-05-13 15:27:13 -0300

    add copy button to text field and change background color

--- convert.js
+++ convert.js
@@ -1,9 +1,9 @@
-var showdown  = require('showdown');
+var showdown = require('showdown');
 var fs = require('fs');
 let filename = process.argv[4] || "README.md"
 let pageTitle = process.argv[2] || ""
 let plausibleDomain = process.argv[3] || ""
-var hljs = require ('highlight.js');
+var hljs = require('highlight.js');
 
 showdown.extension('highlight', function () {
   function htmlunencode(text) {
@@ -12,20 +12,20 @@ showdown.extension('highlight', function () {
         .replace(/&amp;/g, '&')
         .replace(/&lt;/g, '<')
         .replace(/&gt;/g, '>')
-      );
+    );
   }
   return [{
     type: "output",
     filter: function (text, converter, options) {
       var left = "<pre><code\\b[^>]*>",
-          right = "</code></pre>",
-          flags = "g";
+        right = "</code></pre>",
+        flags = "g";
       var replacement = function (wholeMatch, match, left, right) {
         match = htmlunencode(match);
         var lang = (left.match(/class=\"([^ \"]+)/) || [])[1];
         left = left.slice(0, 18) + 'hljs ' + left.slice(18);
         if (lang && hljs.getLanguage(lang)) {
-          return left + hljs.highlight(match, {language: lang}).value + right;
+          return left + hljs.highlight(match, { language: lang }).value + right;
         } else {
           return left + hljs.highlightAuto(match).value + right;
         }
@@ -36,10 +36,10 @@ showdown.extension('highlight', function () {
 });
 
 fs.readFile(__dirname + '/style.css', function (err, styleData) {
-  fs.readFile(__dirname + '/node_modules/highlight.js/styles/atom-one-dark.css', function(err, highlightingStyles) {
+  fs.readFile(__dirname + '/node_modules/highlight.js/styles/atom-one-dark.css', function (err, highlightingStyles) {
     fs.readFile(process.cwd() + '/' + filename, function (err, data) {
       if (err) {
-        throw err; 
+        throw err;
       }
       let text = data.toString();
 
@@ -74,8 +74,54 @@ fs.readFile(__dirname + '/style.css', function (err, styleData) {
           </div>
           <style type='text/css'>` + styleData + `</style>
           <style type='text/css'>` + highlightingStyles + `</style>
+          <script>
+          let blocks = document.querySelectorAll('pre code');
+
+          blocks.forEach((block) => {
+            // Create a copy button element
+            const copyButton = document.createElement("span");
+            copyButton.innerText = "Copy";
+            copyButton.classList.add("copy-button");
+
+            // Append the copy button to the <pre> tag
+            block.appendChild(copyButton);
+
+            // Add click event listener to the copy button
+            copyButton.addEventListener("click", () => {
+              // Hide the copy button temporarily
+              copyButton.style.display = "none";
+            
+              // Create a range and select the text inside the <pre> tag
+              const range = document.createRange();
+              range.selectNode(block);
+              window.getSelection().removeAllRanges();
+              window.getSelection().addRange(range);
+            
+              try {
+                // Copy the selected text to the clipboard
+                document.execCommand("copy");
+              
+                // Alert the user that the text has been copied
+                copyButton.innerText = "Copied!";
+                setTimeout(function(){
+                  copyButton.innerText = "Copy";
+                }, 1500);
+              } catch (err) {
+                console.error("Unable to copy text:", err);
+              } finally {
+                // Show the copy button again
+                copyButton.style.display = "inline";
+              
+                // Deselect the text
+                window.getSelection().removeAllRanges();
+              }
+            });
+          });
+          
+      </script>
         </body>
       </html>`;
+
 
       html = preContent + converter.makeHtml(text) + postContent
 
@@ -84,7 +130,7 @@ fs.readFile(__dirname + '/style.css', function (err, styleData) {
 
       let markdownFileNameWithoutPath = filename.replace(".md", ".html")
       let filePath = process.cwd() + "/" + markdownFileNameWithoutPath
-      fs.writeFile(filePath, html, { flag: "wx" }, function(err) {
+      fs.writeFile(filePath, html, { flag: "wx" }, function (err) {
         if (err) {
           console.log("File '" + filePath + "' already exists. Aborted!");
         } else {
